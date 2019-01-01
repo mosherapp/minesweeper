9d2a2201b8c9d85d981835469377c252898c8340
Moshe Rapp <msr.moshe@gmail.com>
2019-01-01 14:57:58 -0500

    Initial Commit

---
+++ src/minesweeper.js
@@ -0,0 +1,15 @@
+const blankLine = ' | | ';
+const guessLine = '1| | ';
+const bombLine = ' |B| ';
+
+// See what an empty board looks like
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// See what a used board looks like
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
